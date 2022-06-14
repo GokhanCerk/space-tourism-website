d87f263b9b86cb79303e0cd0995c3681b3075074
gokhancerk <devcerk@gmail.com>
2022-06-14 13:31:05 +0300

    destination map json data

--- src/pages/destination/index.js
+++ src/pages/destination/index.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import Navigation from "../../components/navigation";
 import styles from "./index.module.css";
 import { DATA } from "../../data";
-import { useLocation } from "react-router-dom";
 import TabBtn from "../../components/Tabs.js";
 
 const Destination = () => {
-  const { hero, img, container, active, universeImg, tabBtns } = styles;
+  const {
+    hero,
+    img,
+    container,
+    active,
+    universeImg,
+    universeDescription,
+    tabBtns,
+    star,
+  } = styles;
   const [isActive, setIsActive] = useState("Moon");
   const destinations = DATA.destinations;
   console.log(destinations);
@@ -23,20 +31,30 @@ const Destination = () => {
 
   const universe = destinations.map((destination) => {
     return (
-      destination.name === isActive && <img src={destination.images.png} className={img}/>
+      destination.name === isActive && (
+        <img src={destination.images.png} className={img} />
+      )
     );
   });
+
+  const getDescription = (key) => {
+    return destinations.map((destination) => {
+      return destination.name === isActive && destination[key];
+    });
+  };
 
   return (
     <main className={hero}>
       <Navigation />
 
       <section className={container}>
-        <div className={universeImg}>
-            {universe}
-        </div>
-        <div className={tabBtns}>
-          {tabBtn}
+        <div className={universeImg}>{universe}</div>
+        <div className={universeDescription}>
+          <div className={tabBtns}>{tabBtn}</div>
+          <h1 className={star}>{isActive}</h1>
+          <p>{getDescription("description")}</p>
+          <p>{getDescription("distance")}</p>
+          <p>{getDescription("travel")}</p>
         </div>
       </section>
     </main>
